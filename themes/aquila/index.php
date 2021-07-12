d1718d68784ca5ec26ce1c63c07c2c86283f0020
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-07-12 18:43:06 -0300

    #2 lesson was completed

---
+++ themes/aquila/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package Aquila
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Advanced Wordpress Theme Development</title>
+</head>
+<body>
+Hello wordpress
+</body>
+</html>
