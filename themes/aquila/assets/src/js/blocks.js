d7a9a08bf561f1986eb453290377c107adf6135e
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-07-25 01:11:57 -0300

    #79 lesson was completed

--- themes/aquila/assets/src/js/blocks.js
+++ themes/aquila/assets/src/js/blocks.js
@@ -3,5 +3,7 @@ import '../sass/blocks.scss';
 // Blocks
 import './gutenberg/blocks/heading-with-icon';
 import './gutenberg/blocks/dos-and-donts';
-// import './gutenberg/block-extensions/register-block-styles';
+
+// Block extensions
+import './gutenberg/block-extensions/register-block-styles';
 
